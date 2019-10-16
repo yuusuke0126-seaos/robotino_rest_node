cbc5db2f01f3603ea0b8ee62bee988b1903d3f02
Yusuke Kobayashi <y-kobayashi@seaos.co.jp>
2019-10-16 17:56:11 +0900

    Update omnidrive2.py

--- scripts/omnidrive2.py
+++ scripts/omnidrive2.py
@@ -77,6 +77,8 @@ class RobotinoDriver():
         G0.append([cos(pi/4.5*i), sin(pi/4.5*i)])
 #        h0.append((dist_data[i]-0.3)*0.15/0.05)
         h0.append(0.0)
+        if dist_data[i] < 0.3:
+          h0[-1] = -0.05
     G = matrix(numpy.array(G0))
     h = matrix(numpy.array(h0))
     sol=cvxopt.solvers.qp(P,q,G,h)
